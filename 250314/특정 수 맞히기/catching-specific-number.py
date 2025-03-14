16d6ca5430121bb8fa89c07350262ab60fb3f9fb
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-03-14 09:57:48 +0900

    Title: 특정 수 맞히기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250314/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,12 @@
+n = -1
+
+while ( n != 25 ) :
+
+    n = int(input())
+
+    if( n < 25 ) :
+        print("Higher")
+    elif ( n > 25 ) :
+        print("Lower")
+    else :
+        print("Good")
